2b2c38cdc39ef7c101707729b6174f0048bda362
Jonathan Selander <selander.jonathan@gmail.com>
2014-04-22 16:24:16 +0200

    remove json stuff from redis session

--- code/Cache/Redis/Session.php
+++ code/Cache/Redis/Session.php
@@ -149,7 +149,7 @@ class Made_Cache_Redis_Session
             return '';
         }
 
-        return json_decode(gzuncompress($result));
+        return gzuncompress($result);
     }
 
     /**
@@ -164,7 +164,7 @@ class Made_Cache_Redis_Session
         $client = $this->_getClient();
         $client->watch($id);
         $client->multi();
-        $client->set($id, gzcompress(json_encode($data), 6));
+        $client->set($id, gzcompress($data, 6));
         $client->expire($id, time()+$this->_maxLifetime);
         $result = $client->exec();
         if (empty($result[0])) {
