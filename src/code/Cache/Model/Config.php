e1243c974aa11ba2ddf4350fdf02bc4f5b4bf232
Jonathan Selander <jonathan@mondaycreative.se>
2014-09-30 20:53:26 +0200

    experimental config cache regeneration lock

---
+++ src/code/Cache/Model/Config.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Custom config model that leverages a redis lock to prevent config generation
+ * DDOS, typically a problem when you have many store views and a high amount
+ * of visitors.
+ *
+ * Also depends on a local copy of Mage_Core_Model_App put in app/code/local.
+ * The reason for this local override is that the app class _initModules()
+ * needs locking as well, otherwise everything arbitrary will break. Quite
+ * annoying that this is the way it's made, but hey, we have no say.
+ *
+ * @author jonathan@madepeople.se
+ */
+class Made_Cache_Model_Config extends Mage_Core_Model_Config
+{
+
+    /**
+     * Initialization of core configuration
+     *
+     * @return Mage_Core_Model_Config
+     */
+    public function init($options=array())
+    {
+        $this->setCacheChecksum(null);
+        $this->_cacheLoadedSections = array();
+        $this->setOptions($options);
+        $this->loadBase();
+
+        $cacheLoad = $this->loadModulesCache();
+        if ($cacheLoad) {
+            return $this;
+        }
+
+        $backend = Mage::app()->getCacheInstance()
+            ->getFrontend()
+            ->getBackend();
+
+        $options = $this->getLockingOptions();
+
+        // Spin lock
+        $lockSpun = false;
+        while (!$backend->acquireLock($options['lock_name'], $options['token'], $options['timeout'])) {
+            $lockSpun = true;
+            usleep(500000); // 0.5 seconds
+        }
+
+        if ($lockSpun) {
+            // The cache might have been generated while we waited for the lock
+            $cacheLoad = $this->loadModulesCache();
+            if ($cacheLoad) {
+                $backend->releaseLock($options['lock_name'], $options['token']);
+                return $this;
+            }
+        }
+
+        // Check cache
+        $this->loadModules();
+        $this->loadDb();
+        $this->saveCache();
+
+        $backend->releaseLock($options['lock_name'], $options['token']);
+
+        return $this;
+    }
+
+    /**
+     * Parameters used for locking the config cache generation
+     *
+     * @return array
+     */
+    public function getLockingOptions()
+    {
+        $options = array(
+            'lock_name' => 'lock.config_init',
+            'timeout' => 30, // In seconds
+        );
+        $options['token'] = md5($options['lock_name']);
+        return $options;
+    }
+
+}
